ad43194b7e73d1bfca3e7cb11fda6dba3f957823
杨涛 <yangtao@192.168.0.125>
2015-11-17 23:50:25 +0800

    @configuration,propertySource,value 注解使用

--- framework-web/src/main/java/com/yt/config/RedisConf.java
+++ framework-web/src/main/java/com/yt/config/RedisConf.java
@@ -1,10 +1,8 @@
 package com.yt.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 /**
  * 使用spring注解,加载配置文件
