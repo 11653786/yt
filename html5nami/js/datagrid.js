1290b9ffa89bb16291a808c1b28044bde1fcda15
杨涛 <yangtao@yangtaodeMacBook-Air.local>
2015-12-05 18:09:09 +0800

    万用的app.js,顶级模版放在所有的js中app.js的顺序在最后

--- html5nami/js/datagrid.js
+++ html5nami/js/datagrid.js
@@ -1,5 +1,7 @@
 var myapp = angular.module('myapp', ['ui.router', 'ngAnimate']);
 
 var datagrid = myapp.controller('datagridcontroller', function ($scope) {
-    $scope.name = "name";
+    $scope.name = "欢迎来到angularjs,angularjs文档:http://docs.angularjs.cn/api/ng/function/angular.copy";
+    $scope.items = ["item1", "item2", "item3"];
+    $scope.user.name = "angularjs";
 });
