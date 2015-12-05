bc41ae2c9d6c1d15d4d2feb4a90dd994d408bf4b
杨涛 <yangtao@yangtaodeMacBook-Air.local>
2015-12-05 19:18:03 +0800

    万用的app.js,顶级模版放在所有的js中app.js的顺序在最后

--- html5nami/js/app.js
+++ html5nami/js/app.js
@@ -5,7 +5,6 @@
 //temlateUrL真实路径
 //views,一个页面上如果有多个ui-view使用,命名规则:state名@ui-view名
 myapp.config(function ($stateProvider, $urlRouterProvider) {
-    //o
     $urlRouterProvider.otherwise("/hello");
     //匹配ui-sref=state1超链接,的属性路径为/hello,真实地址是html/form.html
     //$apply()方法可以在angular框架之外执行angular JS的表达式，例如：DOM事件、setTimeout、XHR或其他第三方的库。这仅仅是个开始，水还有很深，欢迎大家一起来deep dive！
@@ -16,7 +15,7 @@ myapp.config(function ($stateProvider, $urlRouterProvider) {
             //当前页面的ui-view为''的直接显示html/form.html,也可以理解为设置首页
             '': {
                 templateUrl: "datagrid.html",
-                controller: "datagridcontroller"
+                //controller: "datagridcontroller"
             }
         }
     })
--- html5nami/js/datagrid.js
+++ html5nami/js/datagrid.js
@@ -3,5 +3,31 @@ var myapp = angular.module('myapp', ['ui.router', 'ngAnimate']);
 var datagrid = myapp.controller('datagridcontroller', function ($scope) {
     $scope.name = "欢迎来到angularjs,angularjs文档:http://docs.angularjs.cn/api/ng/function/angular.copy";
     $scope.items = ["item1", "item2", "item3"];
-    $scope.user.name = "angularjs";
+    $scope.user = {name: "name", age: "18"};
+    $scope.user.name = "zhangsan";
+    $scope.member = {name: "zhagnsan1", age1: "16s"};
+    //点击按钮user对象被member对象替换
+
+    $scope.angularcode = function () {
+        angular.copy($scope.member, $scope.user);
+    }
+});
+
+
+//自定义百度标签
+myapp.directive('baidu', function () {
+    return {
+        restrict: 'EAMC',
+        //没有使用scope,从directive中的controller里获取了{{html}}
+        template: function (element, attr) {
+            if (attr.baidu == "google") {
+                return "百度标签,google";
+
+            }else if(attr.baidu=='baidu'){
+                return "百度标签,baidu";
+            }else{
+                return "其他标签,{{name}}";
+            }
+        }
+    }
 });
