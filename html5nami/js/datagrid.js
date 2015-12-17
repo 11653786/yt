422e8f93bc68dca21003c418c63bacc6492df0ab
11653786@qq.com <yangtao>
2015-12-17 18:13:43 +0800

    取消alert

--- html5nami/js/datagrid.js
+++ html5nami/js/datagrid.js
@@ -1,7 +1,4 @@
 var datagrid = myapp.controller('datagridcontroller', function ($scope, userService,$http) {
-    alert(userService.hello());
-    alert(userService.getListByDao());
-    alert(userService.getUserList());
     $scope.name = "欢迎来到angularjs,angularjs文档:http://docs.angularjs.cn/api/ng/function/angular.copy";
     $scope.items = ["item1", "item2", "item3"];
     $scope.user = {name: "name", age: "18"};
