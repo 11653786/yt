b1e6e4738f15ecf314bd5bdfd48aad9f6a72b41b
11653786@qq.com <yangtao>
2015-12-17 18:55:34 +0800

    定时器

--- html5nami/js/user.js
+++ html5nami/js/user.js
@@ -1,8 +1,8 @@
 //只有第一个controller需要写myapp其他的都不需要写
 
-//写法1
-var datagrid = myapp.controller('usercontroller', function ($scope, $interval,$timeout) {
-    $scope.name = "666";
+//写法1,这个userdao是从datagrid中引入的
+var datagrid = myapp.controller('usercontroller', function ($scope, $interval,$timeout,userDao) {
+    $scope.name = userDao.getList();
     $interval(function () {
         console.log("每隔三秒就执行"+new Date());
     }, 3000);
