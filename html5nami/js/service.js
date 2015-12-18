d732a6d6cf461da9ba0503cf40c7482e874c4e0d
11653786@qq.com <yangtao>
2015-12-18 09:52:13 +0800

    userdao1的注入

--- html5nami/js/service.js
+++ html5nami/js/service.js
@@ -1,2 +1,3 @@
 //第一个js
 document.write('<script src="js/datagridservice.js"></script>');
+document.write('<script src="js/userservice.js"></script>');
--- html5nami/js/user.js
+++ html5nami/js/user.js
@@ -1,14 +1,27 @@
 //只有第一个controller需要写myapp其他的都不需要写
 
 //写法1,这个userdao是从datagrid中引入的
-var datagrid = myapp.controller('usercontroller', function ($scope, $interval,$timeout,userDao) {
+var datagrid = myapp.controller('usercontroller', function ($scope, $interval, $timeout, userDao,userDao1) {
     $scope.name = userDao.getList();
+    $scope.userDao1List = userDao1.getList();
     $interval(function () {
-        console.log("每隔三秒就执行"+new Date());
+        console.log("每隔三秒就执行" + new Date());
     }, 3000);
-    $timeout(function(){
-        console.log("定时器3秒后执行："+new Date());
-    },3000);
+    $timeout(function () {
+        console.log("定时器3秒后执行：" + new Date());
+    }, 3000);
+
+    //监控
+    $scope.$watch("age", function (oldAge, newAge,$scope) {
+            if(oldAge>newAge){
+                console.log("年龄越来越小了..");
+            }else if(oldAge<newAge){
+                console.log("年龄越来越大");
+            }else if(oldAge!=null && newAge!=null){
+                $scope.name="两次输入的年龄相同";
+            }
+
+    });
 });
 
 //写法2
