9e6d65db16667d799a129fff96e336a7e9e74d05
11653786@qq.com <yangtao>
2016-02-24 10:46:29 +0800

    min项目创建

---
+++ framework-web/src/main/java/com/yt/controller/rest/PController.java
@@ -0,0 +1,23 @@
+package com.yt.controller.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2016/2/24 0024.
+ */
+@RestController
+@RequestMapping(value = "/p")
+public class PController {
+
+    @Value("${jdbc.password}")
+    private String mysqlPassword;
+
+    @RequestMapping(value = "/values")
+    @ResponseBody
+    public String getValues(){
+        return mysqlPassword;
+    }
+}
