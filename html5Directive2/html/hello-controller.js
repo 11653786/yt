58c73db62aeecba3e7381e4c306ddc6555ea6e63
11653786@qq.com <yangtao>
2016-02-26 13:55:47 +0800

    12312

--- html5Directive2/html/hello-controller.js
+++
@@ -1,8 +0,0 @@
-myapp.controller('prototypecontroller',function($scope,$http){
-    $scope.loaddata1=function(){
-        alert('loaddata1');
-    }
-    $scope.loaddata2=function(){
-        alert('loaddata2');
-    }
-});
