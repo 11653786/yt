f8a2f26b91b89680094a94754516c0128c9cef6b
11653786@qq.com <yangtao>
2016-02-26 14:53:51 +0800

    12312

--- framework-web/src/main/java/com/yt/aop/Proxy.java
+++ framework-web/src/main/java/com/yt/aop/Proxy.java
@@ -1,5 +1,6 @@
 package com.yt.aop;
 
+import com.yt.service.mybatis.entity.Account;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -13,39 +14,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class Proxy {
 
-
-
-
-    @Pointcut("execution(public * com.core.mybatis.base.BaseDao.save(..))")
-    private void saveMethod(){}//定义一个切入点
-
+    @Pointcut("execution(public * com.core.mybatis.base.BaseDao.insert(..))")
+    private void saveMethod() {
+    }//定义一个切入点
 
     /**
      * 用作aop日志
+     *
      * @param point
      * @throws Throwable
      */
     @Around("saveMethod()")
-    public void doBasicProfiling(JoinPoint point) throws Throwable{
+    public void doBasicProfiling(JoinPoint point) throws Throwable {
         System.out.println("in");
-
-        Object[] args=point.getArgs();
-        System.out.println( "getTarget:"+ point.getTarget());
+        Object[] args = point.getArgs();
+        System.out.println("getTarget:" + point.getTarget());
         System.out.println("url+method:" + point.getSignature().getDeclaringTypeName() + "." + point.getSignature().getName());
         for (int i = 0; i < args.length; i++) {
-          Object obj= args[i]; //输出传入的参数
+            Object obj = args[i]; //输出传入的参数
             //判断当前obj类型是不是account类型
-            System.out.println("obj:"+obj);
-//            if(obj instanceof Account){
-//                    System.out.println(Account.class.getName());
-//                    Account account= (Account) obj;
-//                System.out.println(account);
-//            }
+            System.out.println("obj:" + obj);
+            if(obj instanceof Account){
+                    System.out.println(Account.class.getName());
+                    Account account= (Account) obj;
+                System.out.println(account);
+            }
         }
         System.out.println("out");
     }
 
-        //环绕的ProceedingJoinPoint 可以获取返回参数，只有环绕可以使用
+    //环绕的ProceedingJoinPoint 可以获取返回参数，只有环绕可以使用
 //    @Around("execution(public * com.rbao.east.service.sina.impl.DepositSendServiceImpl.balanceFreeze(..))")
 //    private void balanceFreeze(ProceedingJoinPoint point){
 //        Object[] args = point.getArgs();
