43ccfa0b49b84aaefeda9b4806bb0bb40793a7b5
11653786@qq.com <yangtao>
2016-02-29 16:22:02 +0800

    spring xml文件分离

---
+++ framework-web/src/main/java/com/yt/controller/login/LoginController.java
@@ -0,0 +1,13 @@
+package com.yt.controller.login;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 后台登录
+ * Created by Administrator on 2016/2/29 0029.
+ */
+@Controller(value = "/login")
+public class LoginController {
+
+
+}
