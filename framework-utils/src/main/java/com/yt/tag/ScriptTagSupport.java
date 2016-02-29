d43cb30979a0f36e3ef442b959bb5bfda05264b4
11653786@qq.com <yangtao>
2016-02-29 11:10:45 +0800

    java 自定义标签

---
+++ framework-utils/src/main/java/com/yt/tag/ScriptTagSupport.java
@@ -0,0 +1,54 @@
+package com.yt.tag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.JspFragment;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+import java.io.StringWriter;
+
+/**
+ * Created by Administrator on 2016/2/29 0029.
+ */
+public class ScriptTagSupport extends SimpleTagSupport {
+    private int counts;
+
+    public int getCount() {
+        return counts;
+    }
+
+    public void setCount(int count) {
+        this.counts = count;
+    }
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        // TODO Auto-generated method stub
+        JspWriter out = this.getJspContext().getOut();
+        String str = invokeBody();
+        out.println(str.toUpperCase());
+    }
+
+    /**
+     * 拼装标签的方法
+     * @return
+     */
+    private String invokeBody() {
+        // TODO Auto-generated method stub
+        JspFragment body = this.getJspBody();
+        StringWriter sw = new StringWriter();
+        sw.append("hello,world");
+        if (body != null) {
+            try {
+                body.invoke(sw);
+            } catch (JspException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return sw.toString();
+    }
+}
--- framework-web/src/main/webapp/index.jsp
+++ framework-web/src/main/webapp/index.jsp
@@ -6,12 +6,15 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!--引入自定义标签-->
+<%@taglib prefix="simple" uri="/WEB-INF/tag/taglib.tld" %>
 <html>
 <head>
-  <title>22</title>
-  <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resource/css/style.css">
+    <title>22</title>
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resource/css/style.css">
 </head>
 <body>
 你好
+<simple:Script ></simple:Script>
 </body>
 </html>
