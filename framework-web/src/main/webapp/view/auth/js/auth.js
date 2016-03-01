e426a2c35ed4e318c254bc690c32531da7a1a123
11653786@qq.com <yangtao>
2016-03-01 12:12:03 +0800

    login

--- framework-web/src/main/webapp/view/auth/js/auth.js
+++ framework-web/src/main/webapp/view/auth/js/auth.js
@@ -14,26 +14,32 @@ var names = ['Homer', 'Marge', 'Bart', 'Lisa', 'Mo'];
 function createSubTree(level, width, prefix) {
     if (level > 0) {
         var res = [];
-        for (var i=1; i <= width; i++)
-            res.push({ "label" : "Node " + prefix + i, "id" : "id"+prefix + i, "i": i, "children": createSubTree(level-1, width, prefix + i +"."), "name": names[i%names.length] });
+        for (var i = 1; i <= width; i++)
+            res.push({
+                "label": "Node " + prefix + i,
+                "id": "id" + prefix + i,
+                "i": i,
+                "children": createSubTree(level - 1, width, prefix + i + "."),
+                "name": names[i % names.length]
+            });
         return res;
     }
     else
         return [];
 }
 myapp.controller("auth_controller", function ($scope, $http) {
-    $scope.treedata=createSubTree(3, 4, "14");
-    $scope.showToggle = function(node, expanded, $parentNode, $index, $first, $middle, $last, $odd, $even) {
-        var parent = $parentNode?("child of: " + $parentNode.label):"root node";
-        var location = $first?"first":($last?"last":("middle node at index " + $index));
-        var oddEven = $odd?"odd":"even";
-        $("#events-listing").append("<li>"+node.label+ (expanded?" expanded":" collapsed") +" (" + parent + ", " + location +", " + oddEven + ")</li>");
+    $scope.treedata = createSubTree(3, 4, "14");
+    $scope.showToggle = function (node, expanded, $parentNode, $index, $first, $middle, $last, $odd, $even) {
+        var parent = $parentNode ? ("child of: " + $parentNode.label) : "root node";
+        var location = $first ? "first" : ($last ? "last" : ("middle node at index " + $index));
+        var oddEven = $odd ? "odd" : "even";
+        $("#events-listing").append("<li>" + node.label + (expanded ? " expanded" : " collapsed") + " (" + parent + ", " + location + ", " + oddEven + ")</li>");
     };
-    $scope.showSelected = function(node, selected, $parentNode, $index, $first, $middle, $last, $odd, $even) {
-        var parent = $parentNode?("child of: " + $parentNode.label):"root node";
-        var location = $first?"first":($last?"last":("middle node at index " + $index));
-        var oddEven = $odd?"odd":"even";
-        $("#events-listing").append("<li>"+node.label+ (selected?" selected":" deselected") +" (" + parent + ", " + location +", " + oddEven + ")</li>");
+    $scope.showSelected = function (node, selected, $parentNode, $index, $first, $middle, $last, $odd, $even) {
+        var parent = $parentNode ? ("child of: " + $parentNode.label) : "root node";
+        var location = $first ? "first" : ($last ? "last" : ("middle node at index " + $index));
+        var oddEven = $odd ? "odd" : "even";
+        $("#events-listing").append("<li>" + node.label + (selected ? " selected" : " deselected") + " (" + parent + ", " + location + ", " + oddEven + ")</li>");
     };
 });
 
