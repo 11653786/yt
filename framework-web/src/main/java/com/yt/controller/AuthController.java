da7d5a3d11ac62c7c1d881ba52f595f8d96ee2a4
11653786@qq.com <yangtao>
2016-03-02 15:51:35 +0800

    6666

--- framework-web/src/main/java/com/yt/controller/AuthController.java
+++ framework-web/src/main/java/com/yt/controller/AuthController.java
@@ -1,17 +1,25 @@
 package com.yt.controller;
 
+import com.yt.base.BaseAction;
+import com.yt.mybatis.AuthService;
 import com.yt.mybatis.entity.Auth;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 /**
  * Created by Administrator on 2016/3/1 0001.
  */
 @Controller
 @RequestMapping(value = "/api/auth")
-public class AuthController {
+public class AuthController extends BaseAction{
+
+    @Autowired
+    private AuthService authService;
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list() {
@@ -22,6 +30,10 @@ public class AuthController {
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     @ResponseBody
     public String save(Auth auth) {
+        auth.setCreateDate(new Date());
+        auth.setCreateUser(1);
+        auth.setIsEnable("1");
+        authService.insert(auth);
         return "auth/list";
     }
 }
